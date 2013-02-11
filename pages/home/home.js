9c1009a3c4d3067d07eab76c53ea380d2ead5f74
Peacefulsage <satish.grob@hotmail.com>
2013-02-12 11:14:19 +1300

    CodeFiles
    Copied across all the required code files

---
+++ pages/home/home.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    "use strict";
+
+    var viewBox;
+
+    WinJS.UI.Pages.define("/pages/home/home.html", {
+        // This function is called whenever a user navigates to this page. It
+        // populates the page elements with the app's data.
+        ready: function (element, options) {
+            // TODO: Initialize the page here.
+
+            viewBox = $('[data-win-control="WinJS.UI.ViewBox"]');
+            var element1 = viewBox.element;
+
+        }
+    });
+})();
